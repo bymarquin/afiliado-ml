4d91489d1b39925771042f564787c030b13e2215
SS76 <saviosales845@gmail.com>
2026-04-27 20:40:46 -0300

    feat: corrige falha de redirecionamento nos botões VER MAIS
    adiciona script novo no router para corrigir o redirecionamento para o topo da página, e não para o final

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -4,6 +4,15 @@ import { pinia } from '@/stores'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
+
+  scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition
+    } else {
+      return { top: 0 }
+  }
+  },
+
   routes: [
     {
       path: '/',
